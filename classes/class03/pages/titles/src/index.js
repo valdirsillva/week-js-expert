93aecf5ed867b7091e0aa2cef2a6519475f323b8
Valdir Silva <valdipiresba@gmail.com>
2023-03-05 11:15:16 -0300

    desenhando as maos como canvas e interagindo com a aplicacao

---
+++ classes/class03/pages/titles/src/index.js
@@ -0,0 +1,5 @@
+import cardsFactory from "./factories/cardsFactory.js"
+import handleGestureFactory from "./factories/handGestureFactory.js"
+
+await cardsFactory.initalize()
+await handleGestureFactory.initalize()
